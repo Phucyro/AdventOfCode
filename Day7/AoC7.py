60958c4be903384e928f4b1a4a17b731d20cd41d
Hong P Pham <phucyro@gmail.com>
2021-12-27 16:58:20 +0100

    modif

--- Day7/AoC7.py
+++ Day7/AoC7.py
@@ -12,8 +12,6 @@ def parseFile(inputFile):
 
 def findOptimalPosition(positions):
     
-    dictPositionValues = dict()
-    
     minDistance = None
     minPosition = None
     
@@ -25,31 +23,13 @@ def findOptimalPosition(positions):
                 
             distance = HigherPosition - LowerPosition 
                 
-            temp = 0
-                
             for k in range(1, distance+1, 1):
-                totalDistance += k                    
-                temp += k
-                    
-            if i == 0 and j == 1:
-                print("here")
-                print(i)
-                print(positions[j])                    
-                print(HigherPosition)
-                print(LowerPosition)
-                print(distance)
-                print(temp)        
-                
+                totalDistance += k                   
                     
             
         if minDistance == None or totalDistance < minDistance:
             minDistance = totalDistance
             minPosition = positions[i]
-                
-            
-                    
-            
-        
     
     print(minPosition, minDistance)
          
